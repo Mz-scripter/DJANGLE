7e66d9fe7a36eccb144e4cfc7c0e3773e827dbd5
Adeko Muheez <mzscripterx5@gmail.com>
2024-12-21 08:01:43 +0100

    Display total number of results and time of search.

--- search/views.py
+++ search/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import SearchResult
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
+import time
 
 def perform_search(query, result_type=None):
     search_query = SearchQuery(query)
@@ -23,10 +24,15 @@ def search_page(request):
     query = request.GET.get('q')
     result_type = request.GET.get('type')
     results = []
+    total_results = 0
+    search_time = 0
 
     if query:
         # Perform your search logic here
+        start_time = time.time()
         results = perform_search(query, result_type)  # Replace with your actual search function
+        total_results = results.count()
+        search_time = time.time() - start_time
 
     # Pagination Setup
     paginator = Paginator(results, 10)
@@ -37,5 +43,13 @@ def search_page(request):
         paginated_results = paginator.page(1)
     except EmptyPage:
         paginated_results = paginator.page(paginator.num_pages)
+    
+    context = {
+        'results': paginated_results,
+        'query': query,
+        'paginator': paginator,
+        'total_results': total_results,
+        'search_time': search_time
+    }
 
-    return render(request, 'search/base.html', {'results': paginated_results, 'query': query, 'paginator': paginator})
+    return render(request, 'search/base.html', context)
