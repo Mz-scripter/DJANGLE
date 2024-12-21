5ee0b848b8f55971a62045209f5c4e7a4c0df05e
Adeko Muheez <mzscripterx5@gmail.com>
2024-12-21 08:49:09 +0100

    Add auto suggestions for the search bar.

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.search_page, name='search-page')
+    path('', views.search_page, name='search-page'),
+    path('autocomplete/', views.autocomplete, name='autocomplete'),
 ]
